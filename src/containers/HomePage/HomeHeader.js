69fa86d8850a10b433e9960c340cbfbcdcc4fb2d
lainguyentruongvu <truongvuback1t@gmail.com>
2024-05-05 23:41:46 +0700

    update translate header

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -44,8 +44,8 @@ class HomeHeader extends Component {
                 </div>
                 <div className='home-header-banner'>
                     <div className='content-up'>
-                        <div className='title1'>NỀN TẢNG Y TẾ</div>
-                        <div className='title2'>CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+                        <div className='title1'><FormattedMessage id="banner.title1" /></div>
+                        <div className='title2'><FormattedMessage id="banner.title2" /></div>
                         <div className='search'>
                             <i class="fas fa-search"></i>
                             <input type='text' className='' placeholder='Tìm kiếm kkhoa khám bệnh' />
@@ -57,37 +57,37 @@ class HomeHeader extends Component {
                                 <div className='border-img-content'>
                                     <div className='icon-child'></div>
                                 </div>
-                                <div className='text-child'>Khám chuyên khoa</div>
+                                <div className='text-child'><FormattedMessage id="banner.child1" /></div>
                             </div>
                             <div className='option-child'>
                                 <div className='border-img-content'>
                                     <div className='icon-child2'></div>
                                 </div>
-                                <div className='text-child'>Khám từ xa</div>
+                                <div className='text-child'><FormattedMessage id="banner.child2" /></div>
                             </div>
                             <div className='option-child'>
                                 <div className='border-img-content'>
                                     <div className='icon-child3'></div>
                                 </div>
-                                <div className='text-child'>Khám tổng quát</div>
+                                <div className='text-child'><FormattedMessage id="banner.child3" /></div>
                             </div>
                             <div className='option-child'>
                                 <div className='border-img-content'>
                                     <div className='icon-child4'></div>
                                 </div>
-                                <div className='text-child'>Xét nghiệm y học</div>
+                                <div className='text-child'><FormattedMessage id="banner.child4" /></div>
                             </div>
                             <div className='option-child'>
                                 <div className='border-img-content'>
                                     <div className='icon-child5'></div>
                                 </div>
-                                <div className='text-child'>Sức khỏe tinh thần</div>
+                                <div className='text-child'><FormattedMessage id="banner.child5" /></div>
                             </div>
                             <div className='option-child'>
                                 <div className='border-img-content'>
                                     <div className='icon-child6'></div>
                                 </div>
-                                <div className='text-child'>Khám nha khoa</div>
+                                <div className='text-child'><FormattedMessage id="banner.child6" /></div>
                             </div>
                         </div>
                     </div>
