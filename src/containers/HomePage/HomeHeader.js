efc341f0feea83f1ddb5b7de6eb6234c44b0555a
lainguyentruongvu <truongvuback1t@gmail.com>
2024-05-14 21:22:57 +0700

    test

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -55,7 +55,7 @@ class HomeHeader extends Component {
                         <div className='title2'><FormattedMessage id="banner.title2" /></div>
                         <div className='search'>
                             <i className="fas fa-search"></i>
-                            <input type='text' className='' placeholder='Tìm kiếm kkhoa khám bệnh' />
+                            <input type='text' className='' placeholder='Tìm kiếm kkhoa khám bệnh1' />
                         </div>
                     </div>
                     <div className='content-down'>
